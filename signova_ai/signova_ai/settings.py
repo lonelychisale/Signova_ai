312fab7e7a53aa9b37c9190ae1dc40321a39837f
lonelychisale <cis-003-18@must.ac.mw>
2026-05-19 22:50:18 +0200

    Email credentials added to env

--- signova_ai/signova_ai/settings.py
+++ signova_ai/signova_ai/settings.py
@@ -155,5 +155,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "favourgchisale@gmail.com"
-EMAIL_HOST_PASSWORD = "wiha zmox rxve sost"
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")

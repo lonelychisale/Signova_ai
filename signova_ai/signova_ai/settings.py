77cf8bfc084191944f32ed20d1e2b0eceb8d6a4c
Lonely Chisale <cis-003-18@must.ac.mw>
2026-05-21 23:33:51 +0200

    email

--- signova_ai/signova_ai/settings.py
+++ signova_ai/signova_ai/settings.py
@@ -151,8 +151,8 @@ SIMPLE_JWT = {
 }
 
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
